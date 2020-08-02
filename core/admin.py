3a67adecb0f38a479a22410c3bd69ae1e3b7d0a8
jneto <jrweizen@gmail.com>
2020-08-02 17:58:44 -0300

    update admin.py

--- core/admin.py
+++ core/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 
 from .models import Produto, Cliente
 
-admin.site.register(Produto)
-admin.site.register(Cliente)
+# Exibicao do Produto - utilizar snake case
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'preco', 'estoque')
+
+# Exibicao do Cliente
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'sobrenome', 'email')
+
+# Registrando
+admin.site.register(Produto, ProdutoAdmin)
+admin.site.register(Cliente, ClienteAdmin)
+
 
